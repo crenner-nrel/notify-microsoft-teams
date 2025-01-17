1ae65696c6788993047f55ae09b3098d791e6656
Cory Renner <cory.renner@nrel.gov>
2025-01-16 20:13:22 -0600

    Update MSTeams.js

--- src/MSTeams.js
+++ src/MSTeams.js
@@ -280,7 +280,7 @@ class MSTeams {
     const client = new IncomingWebhook(url);
     const response = await client.send(payload);
 
-    if (!response.ok) {
+    if (!response.hasOwnProperty("text")) {
       throw new Error('Failed to send notification to Microsoft Teams.\n' + 'Response:\n' + JSON.stringify(response, null, 2));
     }
   }
